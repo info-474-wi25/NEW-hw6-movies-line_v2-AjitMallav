54688d39702065f8d5a886701233209184ea79be
AjitMallav <aj1tmallav@gmail.com>
2025-02-12 13:43:33 -0800

    Steps 1-5

--- main.js
+++ main.js
@@ -17,37 +17,58 @@ d3.csv("movies.csv").then(data => {
         d.year = +d.title_year;    // Convert year to a number
         d.director = d.director_name;
     });
-
+ 
     // Check your work
-    console.log(data);
+    // console.log(data);
 
     /* ===================== LINE CHART ===================== */
 
     // 3: PREPARE LINE CHART DATA (Total Gross by Year)
     // 3.a: Filter out entries with null gross values
-
+    const cleanLineData = data.filter(d => d.score != null
+        && d.year != null
+        && d.year >= 2010
+        );
 
     // 3.b: Group by and summarize (aggregate gross by year)
-
+    const lineMapData = d3.rollup(cleanLineData,
+        v => d3.sum(v, d => d.gross),
+        d => d.year
+        );
 
     // 3.c: Convert to array and sort by year
-
-
+    const lineData = Array.from(lineMapData,
+        ([year, gross]) => ({ year, gross })
+        )
+        .sort((a,b) => a.year - b.year)
+    ;
     // Check your work
-    // console.log(lineData);
+    console.log(cleanLineData);
 
     // 4: SET SCALES FOR LINE CHART
     // 4.a: X scale (Year)
-
+    let xYear = d3.scaleLinear()
+        .domain([2010, d3.max(lineData, d => d.year)])
+        .range([0, width]);
 
     // 4.b: Y scale (Gross)
-
+    let yGross = d3.scaleLinear()
+        .domain([0, d3.max(lineData, d=> d.gross)])
+        .range([height, 0]);
 
     // 4.c: Define line generator for plotting line
-
+    const line = d3.line()
+        .x(d => xYear(d.year))
+        .y(d => yGross(d.gross));
 
     // 5: PLOT LINE
-
+    svgLine.append(".line")
+        .datum(lineData)
+        .attr("d", line)
+        .attr("stroke", "steelblue")
+        .attr("stroke-width", 2)
+        .attr("fill", "none")
+    ;
 
     // 6: ADD AXES FOR LINE CHART
     // 6.a: X-axis (Year)
